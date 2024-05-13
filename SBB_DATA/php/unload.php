be962bd8e357847e8bd062bced794672172144ba
mvb1999 <148038436+mvb1999@users.noreply.github.com>
2024-05-13 09:50:58 +0200

    test mo

--- SBB_DATA/php/unload.php
+++ SBB_DATA/php/unload.php
@@ -11,17 +11,21 @@ try {
     // Erstellt eine neue PDO-Instanz mit der Konfiguration aus config.php
     $pdo = new PDO($dsn, $username, $password, $options);
 
-    // SQL-Query, um Daten basierend auf dem Standort auszuwählen, sortiert nach Zeitstempel
-    // Verwende ein Fragezeichen (?) anstelle eines benannten Parameters
-    $sql = "SELECT AVG(delay_arrival + delay_departure) AS total_delay, week_day
-            FROM `train_journeys`
-            WHERE actual_arrival >= CURDATE() - INTERVAL 30 DAY GROUP BY week_day";
+    // SQL-Query, um die Anzahl der verspäteten Züge pro Tag und die Summe der Verspätungszeit pro Tag zu erhalten
+    $sql = "SELECT DATE(actual_arrival) AS arrival_date,
+                   COUNT(*) AS num_trains,
+                   SUM(IF(delay_arrival > 0, 1, 0)) AS num_delayed_arrival,
+                   SUM(delay_arrival) AS total_delay_arrival,
+                   SUM(IF(delay_departure > 0, 1, 0)) AS num_delayed_departure,
+                   SUM(delay_departure) AS total_delay_departure
+            FROM train_journeys
+            WHERE actual_arrival >= CURDATE() - INTERVAL 30 DAY
+            GROUP BY arrival_date";
 
     // Bereitet die SQL-Anweisung vor
     $stmt = $pdo->prepare($sql);
 
-    // Führt die Abfrage mit der Standortvariablen aus, die in einem Array übergeben wird
-    // Die Standortvariable ersetzt das erste Fragezeichen in der SQL-Anweisung
+    // Führt die Abfrage aus
     $stmt->execute();
 
     // Holt alle passenden Einträge
